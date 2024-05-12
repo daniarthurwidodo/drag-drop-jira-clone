42849cb807c611ca113dd8635af0ce120a9b55a0
Dani Arthur Widodo <dani.widodo@krom.id>
2024-05-12 13:35:10 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-drop-jira-clone';
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(event.container.data, event.previousIndex, event.currentIndex);
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+    if(event)
+    console.log(this.todo, this.done);
+
+  }
+
 }
